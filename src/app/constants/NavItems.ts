a5c2201644a4931de1f70f1000c571039dc20b4c
jratul <jratul@nate.com>
2024-08-29 18:10:50 +0900

    Update NavItems.ts

--- src/app/constants/NavItems.ts
+++ src/app/constants/NavItems.ts
@@ -1,5 +1,244 @@
 const navItems = [
   {
+    group: "pg",
+    items: [
+      {
+        title: "해시",
+        items: [
+          {
+            name: "폰켓몬",
+            href: "/pg/hash/ponketmon",
+          },
+          {
+            name: "완주하지 못한 선수",
+            href: "/pg/hash/wanjoo",
+          },
+          {
+            name: "전화번호 목록",
+            href: "/pg/hash/phone-number",
+          },
+          {
+            name: "의상",
+            href: "/pg/hash/costume",
+          },
+          {
+            name: "베스트앨범",
+            href: "/pg/hash/bestalbum",
+          },
+        ],
+      },
+      {
+        title: "스택/큐",
+        items: [
+          {
+            name: "같은 숫자는 싫어",
+            href: "/pg/stack-queue/hate-same-num",
+          },
+          {
+            name: "기능개발",
+            href: "/pg/stack-queue/feature-dev",
+          },
+          {
+            name: "올바른 괄호",
+            href: "/pg/stack-queue/valid-paren",
+          },
+          {
+            name: "다리를 지나는 트럭",
+            href: "/pg/stack-queue/truck",
+          },
+          {
+            name: "주식가격",
+            href: "/pg/stack-queue/stock",
+          },
+        ],
+      },
+      {
+        title: "힙",
+        items: [
+          {
+            name: "더 맵게",
+            href: "/pg/heap/hotter",
+          },
+          {
+            name: "디스크 컨트롤러",
+            href: "/pg/heap/disk-controller",
+          },
+          {
+            name: "이중우선순위큐",
+            href: "/pg/heap/dual-priority-queue",
+          },
+        ],
+      },
+      {
+        title: "정렬",
+        items: [
+          {
+            name: "K번째수",
+            href: "/pg/sort/k-num",
+          },
+          {
+            name: "가장 큰 수",
+            href: "/pg/sort/biggest",
+          },
+          {
+            name: "H-Index",
+            href: "/pg/sort/h-index",
+          },
+        ],
+      },
+      {
+        title: "완전탐색",
+        items: [
+          {
+            name: "최소직사각형",
+            href: "/pg/bf/min-square",
+          },
+          {
+            name: "모의고사",
+            href: "/pg/bf/test",
+          },
+          {
+            name: "소수 찾기",
+            href: "/pg/bf/prime",
+          },
+          {
+            name: "카펫",
+            href: "/pg/bf/carpet",
+          },
+          {
+            name: "피로도",
+            href: "/pg/bf/fatigue",
+          },
+          {
+            name: "전력망을 둘로 나누기",
+            href: "/pg/bf/power",
+          },
+          {
+            name: "모음사전",
+            href: "/pg/bf/dict",
+          },
+        ],
+      },
+      {
+        title: "탐욕법",
+        items: [
+          {
+            name: "체육복",
+            href: "/pg/greedy/jersey",
+          },
+          {
+            name: "조이스틱",
+            href: "/pg/greedy/joystick",
+          },
+          {
+            name: "큰 수 만들기",
+            href: "/pg/greedy/big-num",
+          },
+          {
+            name: "구명보트",
+            href: "/pg/greedy/boat",
+          },
+          {
+            name: "섬 연결하기",
+            href: "/pg/greedy/island",
+          },
+          {
+            name: "단속카메라",
+            href: "/pg/greedy/camera",
+          },
+        ],
+      },
+      {
+        title: "동적계획법",
+        items: [
+          {
+            name: "N으로 표현",
+            href: "/pg/dp/n",
+          },
+          {
+            name: "정수 삼각형",
+            href: "/pg/dp/tri",
+          },
+          {
+            name: "등굣길",
+            href: "/pg/dp/school",
+          },
+          {
+            name: "사칙연산",
+            href: "/pg/dp/cal",
+          },
+          {
+            name: "도둑질",
+            href: "/pg/dp/thief",
+          },
+        ],
+      },
+      {
+        title: "DFS/BFS",
+        items: [
+          {
+            name: "타겟 넘버",
+            href: "/pg/dfs-bfs/target-num",
+          },
+          {
+            name: "네트워크",
+            href: "/pg/dfs-bfs/network",
+          },
+          {
+            name: "게임 맵 최단거리",
+            href: "/pg/dfs-bfs/shortcut",
+          },
+          {
+            name: "단어 변환",
+            href: "/pg/dfs-bfs/word",
+          },
+          {
+            name: "아이템 줍기",
+            href: "/pg/dfs-bfs/item",
+          },
+          {
+            name: "여행경로",
+            href: "/pg/dfs-bfs/trip",
+          },
+          {
+            name: "퍼즐 조각 채우기",
+            href: "/pg/dfs-bfs/puzzle",
+          },
+        ],
+      },
+      {
+        title: "이분탐색",
+        items: [
+          {
+            name: "입국심사",
+            href: "/pg/binary/enter",
+          },
+          {
+            name: "징검다리",
+            href: "/pg/binary/bridge",
+          },
+        ],
+      },
+      {
+        title: "그래프",
+        items: [
+          {
+            name: "가장 먼 노드",
+            href: "/pg/graph/further",
+          },
+          {
+            name: "순위",
+            href: "/pg/graph/rank",
+          },
+          {
+            name: "방의 개수",
+            href: "/pg/graph/room",
+          },
+        ],
+      },
+    ],
+  },
+  {
     group: "ps",
     items: [
       {
